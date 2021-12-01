7bc3e2cb487e31c2771c599660c3814b24c9e94c
Wahid Ullah <wahid.ullah.cse@gmail.com>
2021-12-01 16:42:41 +0600

    s

--- balanced_parentisis.py
+++ balanced_parentisis.py
@@ -1,22 +1,53 @@
 def checkBalance(s):
-    mystack = Stack(len(s))
-    if s == "":
+    # mystack = Stack(len(s))
+    # if s == "":
+    #     return True
+    # for c in s:
+    #     if c == "(" or c == "{":
+    #         mystack.push(c)  # push the opening bracket
+    #     else:
+    #         if mystack.is_empty():
+    #             return False
+    #         if c == "{" and mystack.peek() != "}":  # the brackets dont match
+    #             return False
+    #         if c == "(" and mystack.peek() != ")":  # the brackets dont matchs
+    #             return False
+    #         mystack.pop()  # pop matching brackets
+    #
+    # if mystack.is_empty():  # stack must be empty at the end
+    #     return True
+    # return False
+    if s is None:
         return True
-    for c in s:
-        if c == "(" or c == "{":
-            mystack.push(c)  # push the opening bracket
+    stack = []
+    for t in s:
+        if t == ')':
+            try:
+                current = stack.pop()
+                if current != '(':
+                    return False
+            except:
+                return False
+        elif t == '}':
+            try:
+                current = stack.pop()
+                if current != '{':
+                    return False
+            except:
+                return False
+        elif t == ']':
+            try:
+                current = stack.pop()
+                if current != '[':
+                    return False
+            except:
+                return False
         else:
-            if mystack.is_empty():
-                return False
-            if c == "{" and mystack.peek() != "}":  # the brackets dont match
-                return False
-            if c == "(" and mystack.peek() != ")":  # the brackets dont matchs
-                return False
-            mystack.pop()  # pop matching brackets
-
-    if mystack.is_empty():  # stack must be empty at the end
+            stack.append(t)
+    if len(stack) == 0:
         return True
-    return False
+    else:
+        return False
 
 if __name__ == '__main__':
     print(checkBalance("(){}"))
